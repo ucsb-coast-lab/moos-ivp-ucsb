30c6a45f0418a6933cc15b96535292aafc21226e
Sean Gillen <seangillen1887@gmail.com>
2015-01-10 19:32:36 -0500

    fixed BHV_constant altitude, removed FollowCurrent behavior to make merging easier when it's complete

--- src/lib_behavior-test/BHV_ConstantAltitude.h
+++ src/lib_behavior-test/BHV_ConstantAltitude.h
@@ -1,26 +1,4 @@
-/*****************************************************************/
-/*    NAME: Michael Benjamin, Henrik Schmidt, and John Leonard   */
-/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA     */
-/*    FILE: BHV_ConstantDepth.h                                  */
-/*    DATE: Jul 3rd 2005                                         */
-/*                                                               */
-/* This program is free software; you can redistribute it and/or */
-/* modify it under the terms of the GNU General Public License   */
-/* as published by the Free Software Foundation; either version  */
-/* 2 of the License, or (at your option) any later version.      */
-/*                                                               */
-/* This program is distributed in the hope that it will be       */
-/* useful, but WITHOUT ANY WARRANTY; without even the implied    */
-/* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR       */
-/* PURPOSE. See the GNU General Public License for more details. */
-/*                                                               */
-/* You should have received a copy of the GNU General Public     */
-/* License along with this program; if not, write to the Free    */
-/* Software Foundation, Inc., 59 Temple Place - Suite 330,       */
-/* Boston, MA 02111-1307, USA.                                   */
-/*****************************************************************/
- 
-#ifndef BHV_CONSTANT_ALTITUDE_HEADER
+ #ifndef BHV_CONSTANT_ALTITUDE_HEADER
 #define BHV_CONSTANT_ALTITUDE_HEADER
 
 #include "IvPBehavior.h"
@@ -51,12 +29,18 @@ public:
 };
 
 
+#ifdef WIN32
+	// Windows needs to explicitly specify functions to export from a dll
+   #define IVP_EXPORT_FUNCTION __declspec(dllexport) 
+#else
+   #define IVP_EXPORT_FUNCTION
+#endif
+
 extern "C" {
   IVP_EXPORT_FUNCTION IvPBehavior * createBehavior(std::string name, IvPDomain domain) 
   {return new BHV_ConstantAltitude(domain);}
 }
 #endif
 
-#endif
 
 
