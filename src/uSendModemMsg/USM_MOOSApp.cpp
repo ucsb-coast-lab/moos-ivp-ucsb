ebfca4bcf3c1c2944c814b6ad3516152a95e08ac
Sean Gillen <seangillen1887@gmail.com>
2015-01-22 19:08:39 -0500

    added comment header

--- src/uSendModemMsg/USM_MOOSApp.cpp
+++ src/uSendModemMsg/USM_MOOSApp.cpp
@@ -1,4 +1,28 @@
-//this program should allow us to send simple messages out through the modem. it requires iWhoiMicroModem to be running alongside it. it should also be launched in its own window
+/*==========================================================================
+File: USM_MOOSApp.cpp
+Authors: Nick Nidzieko & Sean Gillen
+Date: Jan/22/15
+Origin: Horn Point Laboratory
+Description: Allows users to send modem messages over a WHOI micromodem
+             needs to be running alongside iWhoiMicroModem, it should be
+             run in it's own window
+
+ Copyright 2015 Nick Nidzieko, Sean Gillen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+==========================================================================*/
 #include <iostream>
 #include "USM_MOOSApp.h"
 using namespace std;
