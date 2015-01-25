67322e6f501caffa691e6dbffd801baba62882bf
Sean Gillen <seangillen1887@gmail.com>
2015-01-24 20:34:47 -0500

    added comment header

--- src/pDataWatch/pDataWatch.h
+++ src/pDataWatch/pDataWatch.h
@@ -1,3 +1,29 @@
+/*===================================================================
+File: pDataWatch.cpp
+Authors: Nick Nidzieko & Sean Gillen
+Date: Jan-23-15
+Origin: Horn Point Laboratory
+Description: pDataWatch will constantly watch for data coming in to 
+             determine if it's over or under a certain threshold.
+	     TODO: make a better description. 
+
+Copyright 2015 Nick Nidzieko, Sean Gillen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+===================================================================*/
+
 //pDataWatch looks constantly for data publishes under "SCALAR_VALUE" althought it can later be configured to look 
 //for whatever data is neccessary. it then takes the scalar data and after it gets six vlaues starts to take a
 //running average and checking to aee if it is above or below a threshold determined by the "THRESHOLD" paramter.
